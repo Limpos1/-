c77968f22cc9613ea5b07f6c9627a9aee374891d
Robbie <robbie331@>
2024-07-24 12:26:08 +0900

    Level 1. 4번 요구사항 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -9,52 +9,59 @@ public class App {
         int a,b;
         char operator;
         Scanner sc = new Scanner(System.in);
-        try{
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            a = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            b = sc.nextInt();
-        }catch(InputMismatchException e){
-            System.out.println("올바른 숫자를 입력하세요.");
-            sc.next(); // sc.nextInt()는 버퍼에 입력된 값을 소비시키지 않으므로 sc.next()가 다 처리 후 소비시켜 버퍼를 비워주는 역할을 한다.
-            return;
-        }
+        while(true){
+            try{
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                a = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                b = sc.nextInt();
+            }catch(InputMismatchException e){
+                System.out.println("올바른 숫자를 입력하세요.");
+                sc.next(); // sc.nextInt()는 버퍼에 입력된 값을 소비시키지 않으므로 sc.next()가 다 처리 후 소비시켜 버퍼를 비워주는 역할을 한다.
+                continue;
+            }
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        operator = sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            operator = sc.next().charAt(0);
 
-        int result = 0;
-        switch (operator){
-            case '+':
-            {
-                result = a + b;
-                break;
-            }
-            case '-':
-            {
-                result = a - b;
-                break;
-            }
-            case '*':
-            {
-                result = a * b;
-                break;
-            }
-            case '/':
-            {
-                if(b==0){
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    return;
+            int result = 0;
+            switch (operator){
+                case '+':
+                {
+                    result = a + b;
+                    break;
                 }
-                else
-                    result = a / b;
+                case '-':
+                {
+                    result = a - b;
+                    break;
+                }
+                case '*':
+                {
+                    result = a * b;
+                    break;
+                }
+                case '/':
+                {
+                    if(b==0){
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                        continue;
+                    }
+                    else
+                        result = a / b;
+                    break;
+                }
+                default:{
+                    System.out.println("사칙 연산 기호가 아닙니다 (+, -, *, /) 중 하나를 입력하세요");
+                    continue;
+                }
+            }
+            System.out.println("결과: "+result);
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            if(sc.next().equals("exit")){
                 break;
             }
-            default:{
-                System.out.println("사칙 연산 기호가 아닙니다 (+, -, *, /) 중 하나를 입력하세요");
-                return;
-            }
         }
-        System.out.println("결과: "+result);
+
     }
 }
