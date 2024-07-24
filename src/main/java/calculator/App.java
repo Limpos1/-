5d6747c30e89cb2b9efa8c21e521c56a8b1e447a
Robbie <robbie331@>
2024-07-24 11:38:20 +0900

    요구사항 1 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,15 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        a = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        b = sc.nextInt();
     }
 }
