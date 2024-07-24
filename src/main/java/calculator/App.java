686c93eca62c831a1d96b22230b393d7f34739f5
Robbie <robbie331@>
2024-07-24 19:54:12 +0900

    Level 2. 7번 요구사항 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,68 +8,83 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        int a,b,result;
-        int index=0;
-        char operator;
         Scanner sc = new Scanner(System.in);
+        Calculator cal = new Calculator();
+        int a,b,result,flag;
+        int index=0;
+        double diameter;
+        char operator;
         while(true){
-            try{
-                System.out.print("첫 번째 숫자를 입력하세요: ");
-                a = sc.nextInt();
-                System.out.print("두 번째 숫자를 입력하세요: ");
-                b = sc.nextInt();
-            }catch(InputMismatchException e){ // 두 정수 입력
-                System.out.println("올바른 숫자를 입력하세요.");
-                sc.next(); // sc.nextInt()는 버퍼에 입력된 값을 소비시키지 않으므로 sc.next()가 다 처리 후 소비시켜 버퍼를 비워주는 역할을 한다.
-                continue;
-            }
-
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            operator = sc.next().charAt(0);
-
-            try{
-                result = cal.calculate(a,b,operator);
-            }catch (Exception e){ // calculate 함수에서 에러 발생 시 여기서 받아 처리한다.
-                System.out.println(e.getMessage()); // calculate 함수에서 작성한 에러 메시지를 출력한다.
-                continue;
-            }
-            
-            System.out.println("결과: "+result);
-            index++; // 결과 리스트에 얼마나 저장됐는지 확인하기 위함
-
-            
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if(sc.next().equals("remove")){
-                cal.removeResult(); // 삭제 메서드 활용
-                index--; // 삭제했으므로 인덱스가 하나 줄어든다.
-            }
-
-            System.out.println("인덱스와 값을 넣고 수정하려면 y를 누르세요: ");
-            if(sc.next().charAt(0)=='y'){
-                System.out.println("(첫 번째 인덱스는 1입니다!!!!!)수정 할 인덱스: ");
-                int temp = sc.nextInt();
-                System.out.println("수정 할 값");
-                int temp2 = sc.nextInt();
+            System.out.print("사칙 연산 1번, 원의 넓이 구하기 2번: ");
+            flag = sc.nextInt();
+            if(flag == 1){
                 try{
-                    cal.Setter(temp, temp2);
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    a = sc.nextInt();
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    b = sc.nextInt();
+                }catch(InputMismatchException e){ // 두 정수 입력
+                    System.out.println("올바른 숫자를 입력하세요.");
+                    sc.next(); // sc.nextInt()는 버퍼에 입력된 값을 소비시키지 않으므로 sc.next()가 다 처리 후 소비시켜 버퍼를 비워주는 역할을 한다.
+                    continue;
                 }
-                catch (Exception e){
-                    System.out.println(e.getMessage());
+
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                operator = sc.next().charAt(0);
+
+                try{
+                    result = cal.calculate(a,b,operator);
+                }catch (Exception e){ // calculate 함수에서 에러 발생 시 여기서 받아 처리한다.
+                    System.out.println(e.getMessage()); // calculate 함수에서 작성한 에러 메시지를 출력한다.
+                    continue;
                 }
+
+                System.out.println("결과: "+result);
+                index++; // 결과 리스트에 얼마나 저장됐는지 확인하기 위함
+
+
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                if(sc.next().equals("remove")){
+                    cal.removeResult(); // 삭제 메서드 활용
+                    index--; // 삭제했으므로 인덱스가 하나 줄어든다.
+                }
+
+                System.out.println("인덱스와 값을 넣고 수정하려면 y를 누르세요: ");
+                if(sc.next().charAt(0)=='y'){
+                    System.out.println("(첫 번째 인덱스는 1입니다!!!!!)수정 할 인덱스: ");
+                    int temp = sc.nextInt();
+                    System.out.println("수정 할 값");
+                    int temp2 = sc.nextInt();
+                    try{
+                        cal.Setter(temp, temp2);
+                    }
+                    catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                if(sc.next().equals("inquiry")){
+                    cal.inquiryResults();
+                }
+            } // 1번 선택 시 사칙연산 개시
+            else if(flag==2){
+                System.out.print("원의 반지름을 입력하시오: ");
+                diameter = cal.calculateCircleArea(sc.nextDouble());
+                System.out.println("원의 넓이: "+ diameter);
+                cal.Csetter(diameter); // 원의 넓이를 저장
+                
+                cal.CinquiryResults(); // 원의 넓이 목록 바로 출력
             }
-
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            if(sc.next().equals("inquiry")){
-                cal.inquiryResults();
-            }
-
+            flag = 0;
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             if(sc.next().equals("exit")){
                 break;
             }
         }
+
+
 
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,10 +5,28 @@ import java.util.List;
 
 public class Calculator {
 
-    private List<Integer> list;
+    //-------------------------------------------------------------------------------------------
+    private final List<Integer> list;
+    private final List<Double> circlelist;
+    // final 사용 이유 : 참조형 변수의 경우 final을 사용하면 한번 할당한 주소를 변경하지 못하게한다. 밑에 생성자에서 할당을 받으면 이제 어디서도 이 참조형 변수가 바라보는 주소를 바꿀 수 없다.
+    // 단, final을 사용했다고 해도 내부의 내용(list의 요소들)은 변경 할 수 있다. 단지 다른곳에서 "list = new ArrayList<>()"같이 새로운 주소를 절대 참조하지 못하게 할 뿐이다.
+
+    //-------------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------------
+    static final double PI = Math.PI; // static = 전역변수 객체를 만들지 않고도 클래스 수준으로 모두 공유된다. 즉, 다른 클래스에서 객체 생성 없이 "Claculator.PI" 를 통해 접근할 수 있다. 절대적인 값이므로 static으로 선언하여
+    // 어디서든 편하게 사용하기 위해 선언했다.
+
+    // final = 변수를 상수화 한것으로 파이는 변하면 안되므로 변경할 수 없도록 상수화 하였다.
+    //------------------------------------------------------------------------------------------
 
     public Calculator() {
-        this.list = new ArrayList<Integer>(); // 생성자를 통한 초기화
+        this.list = new ArrayList<Integer>(); 
+        this.circlelist = new ArrayList<Double>();
+        // 생성자를 통한 초기화 final이 선언된 참조형 변수들이기 때문에 더이상 새로운 주소를 할당 할 수 없다.
+    }
+    public double calculateCircleArea(double radius){// 원의 넓이를 구하는 메서드
+        return PI*(radius*radius);
     }
 
     public int calculate(int a, int b,char operator)throws Exception  {// throws Exception -> 이 메서드를 호출한 곳에서 오류를 처리해야 한다는 의미
@@ -43,6 +61,10 @@ public class Calculator {
     public int Getter(int index){
         return list.get(index);
     }
+    public double Cgetter(int index){ // 원의 넓이 리스트 요소를 반환해주는 메서드
+        return circlelist.get(index);
+    }
+
     public void Setter(int index,int num) throws Exception{
         index--;
         if(index<0 || index>list.size()-1){
@@ -51,6 +73,10 @@ public class Calculator {
         else{
             list.set(index,num);
         }
+    }
+    public void Csetter(double num){
+        circlelist.add(num);
+        
     }
 
     public void removeResult(){
@@ -62,6 +88,12 @@ public class Calculator {
 
     public void inquiryResults(){
         for(int i : list){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public void CinquiryResults(){ // 원의 넓이 목록 출력
+        for(double i : circlelist){
             System.out.print(i+" ");
         }
         System.out.println();
